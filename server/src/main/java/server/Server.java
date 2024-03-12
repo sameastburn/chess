98fcfad17b05c450e797cd67943d4b9e8c84a7cb
Sam Eastburn <samteastburn@gmail.com>
2024-03-11 19:33:38 -0600

    bypass autograder bug

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,36 +1,37 @@
 package server;
 
 import com.google.gson.Gson;
+import spark.Service;
 import spark.Spark;
 
 import java.nio.file.Paths;
 
+import static spark.Service.ignite;
+
 public class Server {
-  private void initRoutes() {
+  public int run(int desiredPort) {
+    Service http = ignite();
+
+    http.port(desiredPort);
+
+    http.staticFiles.location("web");
+
     RouteHandler routeHandler = RouteHandler.getInstance();
 
     // leverage ResponseTransfer to automatically JSON-ify our responses
     // remember to still deserialize requests
     Gson gson = new Gson();
 
-    Spark.delete("/db", routeHandler::db, gson::toJson);
-    Spark.post("/user", routeHandler::user, gson::toJson);
-    Spark.post("/session", routeHandler::session, gson::toJson);
-    Spark.get("/game", routeHandler::gameGet, gson::toJson);
-    Spark.post("/game", routeHandler::gamePost, gson::toJson);
-    Spark.delete("/session", routeHandler::sessionDelete, gson::toJson);
-    Spark.put("/game", routeHandler::gamePut, gson::toJson);
-  }
+    http.delete("/db", routeHandler::db, gson::toJson);
+    http.post("/user", routeHandler::user, gson::toJson);
+    http.post("/session", routeHandler::session, gson::toJson);
+    http.get("/game", routeHandler::gameGet, gson::toJson);
+    http.post("/game", routeHandler::gamePost, gson::toJson);
+    http.delete("/session", routeHandler::sessionDelete, gson::toJson);
+    http.put("/game", routeHandler::gamePut, gson::toJson);
 
-  public int run(int desiredPort) {
-    Spark.port(desiredPort);
-
-    Spark.staticFiles.location("web");
-
-    initRoutes();
-
-    Spark.awaitInitialization();
-    return Spark.port();
+    http.awaitInitialization();
+    return http.port();
   }
 
   public void stop() {
